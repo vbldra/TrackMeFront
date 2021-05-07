8caf48476416a7a2b925f98255976c6542d5185c
ilona <ilona.avdiushkina@gmail.com>
2021-05-07 13:32:28 +0200

    reducer func to handle events with dispatch

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -9,18 +9,36 @@ import HomeContainer from "../homeContainer/HomeContainer";
 export const AppContext = React.createContext(null);
 
 function App() {
+    // starting point
     const initialEventsState = [
         { id: 1, name: "consumable" },
         { id: 2, name: "mental" },
         { id: 3, name: "social" },
         { id: 4, name: "physical" },
     ];
-    const [events, setEvents] = useReducer(initialEventsState);
+    const [events, dispatch] = useReducer(eventsReducer, initialEventsState);
     const [popupProfileVisible, setPopupProfileVisible] = useState(false);
+
+    // function to handle events
+    function eventsReducer (state, action) {
+        switch (action.type) {
+            case "addNew":
+                return state;
+            default:
+                throw new Error();
+        }
+    };
 
     return (
         <div className="App">
-            <AppContext.Provider value={{events, popupProfileVisible, setPopupProfileVisible}}>
+            <AppContext.Provider
+                value={{
+                    events,
+                    dispatch,
+                    popupProfileVisible,
+                    setPopupProfileVisible,
+                }}
+            >
                 <Navbar />
                 <HomeContainer />
             </AppContext.Provider>
